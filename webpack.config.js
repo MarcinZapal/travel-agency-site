a7aa17a7c46f9342e0d4ac5bc22add80d76f23dc
Marcin <marcin.zapal@gmail.com>
2020-04-23 07:43:46 +0200

    add automation for css and install postcss plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
 const path = require('path')
+const postCSSPlugins=[
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+    ]
 
 module.exports = {
     entry: './app/assets/scripts/App.js',
@@ -7,5 +12,13 @@ module.exports = {
         path: path.resolve(__dirname, 'app')
     },
     mode: 'development',
-    watch: true
+    watch: true,
+    module:{
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
+            }
+        ]
+    }
 }
